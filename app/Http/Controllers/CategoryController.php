764b474c8661a0839a405d6831ce977627dc4afb
abhannouni <abdeslam.hannouni@gmail.com>
2024-10-07 17:08:03 +0100

    Implement Controller Methods

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\CategoryService;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    protected $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->only(['name', 'parent_id']);
+        $this->categoryService->createCategory($data);
+
+        return response()->json(['message' => 'Category created']);
+    }
+
+    public function delete($id)
+    {
+        $this->categoryService->deleteCategory($id);
+        return response()->json(['message' => 'Category deleted']);
+    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ProductService;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    protected $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->only(['name', 'description', 'price', 'image', 'category_id']);
+        $this->productService->createProduct($data);
+
+        return response()->json(['message' => 'Product created']);
+    }
+
+    public function browse(Request $request)
+    {
+        $filters = $request->only(['category']);
+        $sortBy = $request->input('sortBy', 'name');
+        $products = $this->productService->getAllProducts($filters, $sortBy);
+
+        return response()->json($products);
+    }
 }
